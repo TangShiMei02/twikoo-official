625b266bb110351d8bbdecf123c8c0bb10786a8a
TangShiMei02 <2244182951@qq.com>
2025-12-02 19:29:48 +0800

    2025 12 02 19 29

--- api/twikoo.js
+++ api/twikoo.js
@@ -1,2 +1,6 @@
-import twikoo from 'twikoo/dist/twikoo.cjs.min.js'
-export default twikoo
+// ✅ 纯 CommonJS 写法，Vercel 最兼容
+const twikoo = require('twikoo');
+
+module.exports = (req, res) => {
+  return twikoo(req, res);
+};
