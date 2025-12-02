7efd06afd15222c42c99e51f693c11c05422e163
TangShiMei02 <2244182951@qq.com>
2025-12-02 19:52:39 +0800

    update

--- api/twikoo.js
+++ api/twikoo.js
@@ -1,6 +1,4 @@
+const twikoo = require('twikoo');
 module.exports = (req, res) => {
-  res.json({
-    MONGODB_URI: process.env.MONGODB_URI ? '✅ 已设置' : '❌ 未设置',
-    TWIKOO_ADMIN_EMAIL: process.env.TWIKOO_ADMIN_EMAIL || '未设置'
-  });
+  return twikoo(req, res);
 };
